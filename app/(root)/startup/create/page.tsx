8ebc95bc88de9ddb5c518c73623c3ed29dcf6d61
Matias Krummel <matikrummel.l.2004@gmail.com>
2025-03-26 00:08:10 -0300

    Author authentication: new request in 'queries.ts' file for Github users along with 3 callbacks (signIn, jwt, session) in 'auth.ts' file

---
+++ app/(root)/startup/create/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+    <section className='pink_container !min-h-[230px]'>
+      <h1 className='heading'>Submit Your Startup</h1>
+      {/* TODO: STARTUP FORM */}
+    </section>
+    </>
+  )
+}
+
+export default page
